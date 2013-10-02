a1827d65b19c0e5b8c0a39ff471151734b94bb3d
alucasNearsoft <alucas@nearsoft.com>
2013-10-02 10:35:05 -0700

    JSON format
    Added lines in InventoryController and POM dependency for JSON format

--- src/main/java/com/companyname/springapp/web/InventoryController.java
+++ src/main/java/com/companyname/springapp/web/InventoryController.java
@@ -1,8 +1,11 @@
 package com.companyname.springapp.web;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -11,11 +14,17 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.companyname.springapp.domain.Product;
 import com.companyname.springapp.service.ProductManager;
 
 @Controller
@@ -37,8 +46,22 @@ public class InventoryController {
         Map<String, Object> myModel = new HashMap<String, Object>();
         myModel.put("now", now);
         myModel.put("products", this.productManager.getProducts());
+        
+        // json format / formato json
+        final OutputStream out = new ByteArrayOutputStream();
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(out, this.productManager.getProducts());
+        final byte[] data = ((ByteArrayOutputStream) out).toByteArray();
+        myModel.put("productsJson", new String(data));
 
         return new ModelAndView("hello", "model", myModel);
+    }
+    
+    @RequestMapping(value = "/productsjson", method = RequestMethod.GET)
+    public @ResponseBody
+     List<Product> getProductsJson()
+      throws JsonGenerationException, JsonMappingException, IOException {
+        return this.productManager.getProducts();
     }
     
     public void setProductManager(ProductManager productManager) {
